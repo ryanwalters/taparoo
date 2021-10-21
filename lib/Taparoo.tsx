2783d38ce0afd8ac86722d8e5754a8bdbd681d1e
Ryan Walters <ryan@ryanwalters.dev>
2021-10-21 16:17:46 -0500

    fix: prevent single-touch events during multi-touch gestures

--- lib/Taparoo.tsx
+++ lib/Taparoo.tsx
@@ -1,21 +1,23 @@
-import React, { FC, TouchEvent, useState } from 'react';
+import React, { FC, TouchEvent, useEffect, useState } from 'react';
 
-interface TaparooProps {
-  className?: string;
-  onSwipe?: Function;
-  onSwipeUp?: Function;
-  onSwipeDown?: Function;
-  onSwipeLeft?: Function;
-  onSwipeRight?: Function;
-  onTap?: Function;
-  swipeThreshold?: number;
-}
+type TaparooTouchEventHandler = (event: TouchEvent, swipes?: Array<SwipeDirection>) => void;
 
 export enum SwipeDirection {
   Up = 'swipeUp',
   Down = 'swipeDown',
   Left = 'swipeLeft',
   Right = 'swipeRight',
+}
+
+interface TaparooProps {
+  className?: string;
+  onSwipe?: TaparooTouchEventHandler;
+  onSwipeUp?: TaparooTouchEventHandler;
+  onSwipeDown?: TaparooTouchEventHandler;
+  onSwipeLeft?: TaparooTouchEventHandler;
+  onSwipeRight?: TaparooTouchEventHandler;
+  onTap?: TaparooTouchEventHandler;
+  swipeThreshold?: number;
 }
 
 export const Taparoo: FC<TaparooProps> = ({
@@ -32,9 +34,27 @@ export const Taparoo: FC<TaparooProps> = ({
   const [touchMoved, setTouchMoved] = useState<boolean>(false);
   const [startX, setStartX] = useState<number>(0);
   const [startY, setStartY] = useState<number>(0);
+  const [touchCount, setTouchCount] = useState<number>(0);
+  const [isMultiTouchGesture, setIsMultiTouchGesture] = useState<boolean>(false);
+
+  // Determine if the current gesture is multi-touch
+
+  useEffect(() => {
+    if (touchCount > 1) {
+      setIsMultiTouchGesture(true);
+    }
+
+    if (touchCount === 0) {
+      setIsMultiTouchGesture(false);
+    }
+  }, [touchCount]);
+
+  // Touch handlers
 
   function handleTouchStart(event: TouchEvent) {
     const { clientX, clientY } = event.touches[0];
+
+    setTouchCount(event.touches.length);
 
     setStartX(clientX);
     setStartY(clientY);
@@ -47,42 +67,40 @@ export const Taparoo: FC<TaparooProps> = ({
   }
 
   function handleTouchEnd(event: TouchEvent) {
-    if (!touchMoved) {
-      onTap?.(event);
+    if (!isMultiTouchGesture) {
+      detectSingleTouchGestures(event);
     }
 
-    detectSwipes(event);
-
+    setTouchCount(event.touches.length);
     setTouchMoved(false);
     setStartX(0);
     setStartY(0);
   }
 
-  function detectSwipes(event: TouchEvent) {
+  // Detect single-touch gestures
+
+  function detectSingleTouchGestures(event: TouchEvent) {
+    // Tap
+
+    if (!touchMoved) {
+      onTap?.(event);
+    }
+
     const { clientX, clientY } = event.changedTouches[0];
     const deltaX = startX - clientX;
     const deltaY = startY - clientY;
-    const swipes = [];
-
-    console.log(event);
-
-    // Return early if this interaction is not a single-touch event
-    // (e.g. multi-touch events like pinch and zoom)
-
-    if (event.changedTouches.length !== 1) {
-      return;
-    }
+    const swipes: Array<SwipeDirection> = [];
 
     // Horizontal swipe
 
     if (deltaX > swipeThreshold) {
       swipes.push(SwipeDirection.Left);
 
-      onSwipeLeft?.(event);
+      onSwipeLeft?.(event, swipes);
     } else if (deltaX < -swipeThreshold) {
       swipes.push(SwipeDirection.Right);
 
-      onSwipeRight?.(event);
+      onSwipeRight?.(event, swipes);
     }
 
     // Vertical swipe
@@ -90,17 +108,17 @@ export const Taparoo: FC<TaparooProps> = ({
     if (deltaY > swipeThreshold) {
       swipes.push(SwipeDirection.Up);
 
-      onSwipeUp?.(event);
+      onSwipeUp?.(event, swipes);
     } else if (deltaY < -swipeThreshold) {
       swipes.push(SwipeDirection.Down);
 
-      onSwipeDown?.(event);
+      onSwipeDown?.(event, swipes);
     }
 
     // General, horizontal or vertical, swipe
 
     if (swipes.length > 0) {
-      onSwipe?.(swipes, event);
+      onSwipe?.(event, swipes);
     }
   }
 
